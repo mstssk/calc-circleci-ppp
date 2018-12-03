d99477d089e6fa8c9eaea411713fab923f8e8190
mstssk <mstssk+github@gmail.com>
2018-12-03 20:27:44 +0900

    Make CLI

---
+++ cli.js
@@ -0,0 +1,15 @@
+const calcSum = require('./calc');
+
+
+
+const activeUser = parseInt(process.argv[2]);
+const minAndResClasses = [];
+for (let i = 3; process.argv.length > i; i++) {
+    const arg = process.argv[i];
+    const splited = arg.split(":");
+    const resClass = splited[0];
+    const min = parseInt(splited[1]);
+    minAndResClasses.push({ resClass, min });
+}
+
+console.log(calcSum(activeUser, ...minAndResClasses));
