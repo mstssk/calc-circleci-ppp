7b529a1f3325cad7e8c3f1b214cd23777b6597bc
mstssk <mstssk+github@gmail.com>
2019-07-19 00:36:46 +0900

    'baseUrl' is deprecated since Vue CLI 3.3, please use publicPath instead.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production' ? '/calc-circleci-ppp/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/calc-circleci-ppp/' : '/',
 }
